586a1959744e91c2462961db4b9142aad0f8ebb8
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-07-22 21:53:04 -0300

    fix server error in deploy

--- src/app/(ui)/profile/page.tsx
+++ src/app/(ui)/profile/page.tsx
@@ -1,7 +1,7 @@
+"use client"
 import { Profile } from "@/components/layout/profile/profile";
 import { Header } from "@/components/layout/header";
 import { useUsers } from "@/utils/useUsers";
-import { userType } from "@/data/users";
 
 export default function Page() {
   const { users: usersData, loading: loadingUsers } = useUsers();
